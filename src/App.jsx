dfc74bef3f397b61d59c15489fd47883200f8e61
IamShahzaibAli-SA <243137271+IamShahzaibAli-SA@users.noreply.github.com>
2025-11-22 13:43:48 +0500

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>Hello World</p>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
